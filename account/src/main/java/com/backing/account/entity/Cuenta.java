66f92d5ed7bae93c8291029bc8fd9c0dab740364
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-27 17:50:07 +0000

    Eliminar el método setId de la clase Cuenta

--- account/src/main/java/com/backing/account/entity/Cuenta.java
+++ account/src/main/java/com/backing/account/entity/Cuenta.java
@@ -46,10 +46,6 @@ public class Cuenta {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNumeroCuenta() {
         return numeroCuenta;
     }
