5f16e7dd1bbd44e0b28da6915e5cf7bafbc2c475
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-27 19:50:08 +0000

    Actualizar excepción CuentaNotFoundException para aceptar un mensaje personalizado

--- account/src/main/java/com/backing/account/exception/CuentaNotFoundException.java
+++ account/src/main/java/com/backing/account/exception/CuentaNotFoundException.java
@@ -2,9 +2,11 @@ package com.backing.account.exception;
 
 public class CuentaNotFoundException extends RuntimeException {
 
-    public CuentaNotFoundException(Long id) {
-        super("Cuenta con id " + id + " no encontrada");
+    public CuentaNotFoundException(String message) {
+        super(message);
     }
+
+    
     
 
 }
