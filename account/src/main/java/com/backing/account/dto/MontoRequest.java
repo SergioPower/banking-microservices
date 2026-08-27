20d0d62b3f6d827e9b3510f0a2241161db408fd4
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-27 19:01:16 +0000

    Agregar la clase MontoRequest para manejar solicitudes de monto con validaciones

---
+++ account/src/main/java/com/backing/account/dto/MontoRequest.java
@@ -0,0 +1,18 @@
+package com.backing.account.dto;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record MontoRequest(
+
+        @NotNull @Positive BigDecimal monto
+) {
+    @JsonCreator
+    public MontoRequest {
+    }
+
+}
