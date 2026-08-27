7f1cf1499faaec988a05de1e509c400893006657
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-27 16:41:30 +0000

    Agregar clase CuentaResponse para representar la respuesta de la cuenta

---
+++ account/src/main/java/com/backing/account/dto/CuentaResponse.java
@@ -0,0 +1,17 @@
+package com.backing.account.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.backing.account.entity.TipoCuenta;
+
+public class CuentaResponse {
+    Long id;
+    String numeroCuenta;
+    String titular;
+    TipoCuenta tipoCuenta;
+    BigDecimal saldo;
+    Boolean activa;
+    LocalDateTime fechaCreacion;
+
+}
